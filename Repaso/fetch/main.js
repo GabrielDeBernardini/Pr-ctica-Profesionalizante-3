11fca9a3cd1e7281f9113764fc1e116b9c6f38eb
GabrielDeBernardini <gabriel474@hotmail.com>
2023-09-25 19:26:47 -0300

    Subo mas actualizacion

--- Repaso/fetch/main.js
+++ Repaso/fetch/main.js
@@ -1,11 +1,8 @@
 async function solicitar(){
-    const lista = await fetch ("")
-
-    //Convertir los datos recibidos en una expresion nativa de javascript
-    const lista_definitiva = await lista.json()
-    
-    lista_definitiva.array.forEach(element => {
-        console.log(element)
-    });
-
+  const lista = await fetch("https://dolarapi.com/v1/dolares")  
+  const lista_definitiva = await lista.json()
+  lista_definitiva.forEach((element, index) => {
+    console.log(`${index+1} - Dólar ${element.nombre} - Compra: $${element.compra} - Venta: $${element.venta} 
+        `)
+  });
 }
